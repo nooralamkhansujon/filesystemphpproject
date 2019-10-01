99a14ae804ca376da41f20a794aea9dc2251f599
nooralam <nooralam@gmail.com>
2019-10-01 14:43:24 +0600

    upload and remove function done

--- action.php
+++ action.php
@@ -59,5 +59,86 @@ if(isset($_POST['action']))
 
           echo $output;
     }
+
+    if($_POST['action'] == 'create')
+    {
+        if(!file_exists($_POST['folder_name']))
+        {
+           mkdir($_POST['folder_name']);
+           echo "Folder Created";
+        }
+        else{
+            echo "Folder Already Created";
+        }
+    }
+
+    if($_POST['action'] == 'change')
+    {
+         if(!file_exists($_POST['folder_name']))
+         {
+           rename($_POST['old_name'],$_POST['folder_name']);
+           echo "Folder Name Change";
+         }
+         else{
+               echo "Folder Already Created";
+         }
+         
+    }
+
+    if($_POST['action'] == 'fetch_files')
+    {
+        $file_data  = scandir($_POST['folder_name']);
+
+        $output     = '
+           <table class="table table-bordered table-striped">
+              <tr> 
+                 <th>Image</th>
+                 <th>File Name</th>
+                 <th>Delete</th>
+              </tr>';
+
+        foreach($file_data as $file)
+        {
+            if($file  === '.' || $file === '..' )
+            {
+                 continue;
+            }
+            else{
+               
+                 $path    = $_POST['folder_name'].'/'.$file;
+                 $output .='
+                    <tr>
+                       <td>
+                         <img src="'.$path.'" class="img-thumbnail" height="50" width="50" />
+                       </td>
+                       <td>'.$file.' </td>
+                       <td>
+                          <button name="remove_file" class="remove_file btn btn-danger btns-xs" id="'.$path.'">
+                            Remove
+                          </button>
+                       </td>
+                    </tr>';
+            }
+        }      
+
+        $output .= "</table>";
+        echo $output;
+    }
+
+    if($_POST['action'] == 'remove_file')
+    {
+        if(file_exists($_POST['path']))
+        {
+            if(!is_dir($_POST['path']))
+            {
+                    unlink($_POST['path']);
+                    echo "File Deleted";
+            }
+            else{
+                 rmdir($_POST['path']);
+                 echo "Folder is deleted";
+            }   
+        }
+    }
 }
 ?>
--- index.php
+++ index.php
@@ -29,8 +29,8 @@
 </body>
 </html>
 
-<div id="folderModal" class="modal fade" role="dialog">
-    <div class="model-dialog">
+<div id="folderModal" class="modal fade"  role="dialog">
+    <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
              <button type="button" class="close" data-dismiss="modal">&times;</button>
@@ -40,16 +40,67 @@
           </div>
           <div class="modal-body">
             <p>Enter Folder Name
-            <input type="text" name="folder_name" id="folder_name"></p>
+            <input type="text" class="form-control" name="folder_name" id="folder_name"></p>
             <br />
              <input type="hidden" name="action" id="action" />
              <input type="hidden" name="old_name" id="old_name" />
              <input type="button" name="folder_button" id="folder_button" class="btn btn-info" value="Create" />
           </div>
+          <div class="modal-footer">
+             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
         </div>
     </div>
 </div>
 
+
+
+<div id="uploadModal" class="modal fade"  role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+             <button type="button" class="close" data-dismiss="modal">&times;</button>
+             <h4 class="modal-title">
+                 <span id="change_title">Upload Folder</span>
+             </h4>
+          </div>
+          <div class="modal-body">
+              <form  id="upload_form" enctype="multipart/form-data">
+                  <p>Select Image
+                     <input type="file" name="upload_file" />
+                  </p>
+                  <br /> 
+                  <input type="hidden" name="hidden_folder_name" id="hidden_folder_name" />
+                  <input type="submit" name="upload_button" class="btn btn-info" value="Upload" />
+              </form>
+          </div>
+          <div class="modal-footer">
+             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+    </div>
+</div>
+
+
+
+<div id="filelistModal" class="modal fade"  role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+             <button type="button" class="close" data-dismiss="modal">&times;</button>
+             <h4 class="modal-title">
+                 <span id="change_title">File List</span>
+             </h4>
+          </div>
+          <div class="modal-body" id="file_list">
+                 
+          </div>
+          <div class="modal-footer">
+             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+    </div>
+</div>
 
 
 
@@ -70,5 +121,114 @@
                  }
              })
        }
+      
+      // This will create new folder 
+      $(document).on('click','#create_folder',function(){
+          $("#action").val('create');
+          $("#folder_name").val('');
+          $("#folder_button").val('Create');
+          $('#old_name').text('Create Folder');
+          $("#folderModal").modal('show');
+      });
+     
+     // This is folder button click
+      $(document).on('click','#folder_button',function(){
+          let folder_name = $("#folder_name").val();
+          let action      = $("#action").val();
+          let old_name    = $("#old_name").val();
+          if(folder_name != '')
+          {
+               $.ajax({
+                   url   :"action.php",
+                   method:"POST",
+                   data  :{folder_name:folder_name,old_name:old_name,action:action},
+                   success:function(data){
+                       $("#folderModal").modal('hide');
+                       load_folder_list();
+                       alert(data);
+                   }
+               })
+          }
+          else{
+              alert("Enter Folder Name");
+          }
+      });
+     
+     // this is update folder name 
+     $(document).on('click','.update',function(){
+            let folder_name = $(this).data('name');
+            $("#old_name").val(folder_name);
+            $("#folder_name").val(folder_name);
+            $("#action").val("change");
+            $("#folder_button").val('Update');
+            $("#change_title").text("Change Folder Name");
+            $("#folderModal").modal('show');
+     });
+
+     $(document).on('click','.upload',function(){
+
+         let folder_name = $(this).data('name');
+         $("#hidden_folder_name").val(folder_name);
+         $("#uploadModal").modal('show');
+
+     });
+     $("#upload_form").on('submit',function(){
+          $.ajax({
+              url     : "upload.php",
+              method  : "POST",
+              data    : new FormData(this),
+              contentType : false,
+              cache       : false,
+              processData : false,
+              success     : function(data)
+              {
+                load_folder_list();
+                alert(data);
+              }
+
+          });
+     });
+
+      $(document).on('click','.view_files',function(){
+            
+            let folder_name = $(this).data("name");
+            // console.log(folder_name);
+            let action = "fetch_files";
+            $.ajax({
+                url    : "action.php",
+                method : "POST",
+                data   : {action:action,folder_name:folder_name},
+                success:function(data)
+                {
+                    console.log(data);
+                    $("#file_list").html(data);
+                    $("#filelistModal").modal('show');
+                }
+
+            })
+      });
+
+      $(document).on('click','.remove_file',function(){
+            let path = $(this).attr('id');
+            let action = "remove_file";
+            if(confirm("Are You sure you want to remove this File?"))
+            {
+                  $.ajax({
+                      url:"action.php",
+                      method:"POST",
+                      data:{path:path,action:action},
+                      success:function(data)
+                      {
+                          alert(data);
+                          $("#filelistModal").modal('hide');
+                          load_folder_list();
+                      }
+                  })
+            }
+            else{
+                return false;
+            }
+      });
+
   });
 </script>
